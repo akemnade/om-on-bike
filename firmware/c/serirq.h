679c227805315c0ff5508e6f959ab0199feda2c7
Andreas Kemnade <andreas@kemnade.info>
2015-03-27 21:36:06 +0100

    add missing file

---
+++ firmware/c/serirq.h
@@ -0,0 +1,8 @@
+extern uint8_t serfifowritepos;
+extern uint8_t serfiforeadpos;
+extern uint8_t serfifobuf[64];
+extern uint32_t tmr;
+extern uint8_t tmrb2;
+extern uint8_t tfound;
+extern uint8_t pulse_pos;
+
